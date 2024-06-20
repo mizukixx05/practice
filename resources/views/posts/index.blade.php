6ffd4d47e6e14dc226796370e8aaa2bf0b6a3819
mizukixx05 <msu98188@outlook.jp>
2024-06-20 19:51:24 +0900

    ブログ投稿一覧表示条件変更課題OK

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body>
+       <h1>Blog Name</h1>
+      
+          <div clss="posts">         <!--記事一覧-->
+           <div class="post">      　<!--その中のtitle1の事-->
+            <h2 class="Title">Title1</h2> 　<!--タイトル2-->
+                <p class="body">title1のサンプル中身</p>
+       </div>
+       </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\PostController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () 
- { return view('welcome'); });
+Route::get('/', function () {
+ return view('posts.index'); 
+});
  
- Route::get('/posts', [PostController::class, 'index']); 
