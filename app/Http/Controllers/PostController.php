1dbef6472499f734adb7296c474ac3b188c41eaf
mizukixx05 <msu98188@outlook.jp>
2024-06-23 15:42:58 +0900

    データ取得＆表示応用、Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts.index')->with(['posts' => $post->get()]);
+    return view('posts.index')->with(['posts' => $post->getpaginateByLimit(1)]);
     }  //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
 }
